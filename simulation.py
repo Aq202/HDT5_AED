b9542684776085aaf9ebac5e822c8d8a48ab623d
Diego <diegoguatedb2002@gmail.com>
2022-03-14 13:59:26 -0600

    Documentación agregada

--- simulation.py
+++ simulation.py
@@ -1,8 +1,13 @@
+"""
+Hoja de trabajo #5
+Autor: Diego Andrés Morales Aquino
+Fecha: 13/03/2021
+"""
+
 import simpy as sp
 import random
 import numpy as np
 import pyperclip
-
 
 class OperativeSystem:
 
@@ -15,16 +20,15 @@ class OperativeSystem:
         self.tasksExecutionCapacity = tasksExecutionCapacity
         self.executionsRecord = []
 
-        self.resume = self.env.event()
-
     def newProcess(self, memoryWeight, pendingTasks, arrivalDelay=0):
 
-        yield self.env.timeout(arrivalDelay)
-        startTime = self.env.now
+        yield self.env.timeout(arrivalDelay) #delay de ejecución
+        startTime = self.env.now 
 
         print(
             f"[NEW] Esperando asignacion de memoria, en t={self.env.now}, cantidad de memoria requerida={memoryWeight}, memoria disponible={self.ram.level}")
 
+        #Solicitar memoria RAM
         yield self.ram.get(memoryWeight)
 
         while pendingTasks > 0:
@@ -32,11 +36,14 @@ class OperativeSystem:
             print(
                 f"[READY] Proceso en cola , t={self.env.now}, instrucciones pendientes={pendingTasks}")
 
+            #Solicitar Unidad de procesamiento
             with self.cpu.request() as cpu:
                 yield cpu
 
+                #Ejecutar tareas
                 pendingTasks = pendingTasks - self.tasksExecutionCapacity
                 pendingTasks = pendingTasks if pendingTasks > 0 else 0
+
                 yield self.env.timeout(1)  # ciclo de reloj
 
                 print(
@@ -55,6 +62,7 @@ class OperativeSystem:
         # liberar memoria
         self.ram.put(memoryWeight)
 
+        #calcular tiempo de ejecución
         endTime = self.env.now
         executionTime = endTime - startTime
         self.executionsRecord.append(executionTime)
@@ -63,29 +71,35 @@ class OperativeSystem:
             f"[TERMINATED] Proceso finalizado en t={endTime}, tiempo de ejecucion={executionTime}, memoria liberada={memoryWeight}, memoria disponible={self.ram.level}")
 
 
+# Ejecucion de la simulacion
 if __name__ == "__main__":
 
     random.seed(2002)
 
+    # parámetros de ejecución
+
     NUMBER_OF_PROCESSES = 25
-    INTERVAL = 1
+    INTERVAL = 10
     RAM = 100
     TASK_EXECUTION_CAPACITY = 3
     CPU_NUMBER = 1
 
+    #Creacion de simulacion
     env = sp.Environment()
     OS = OperativeSystem(
         env, ramCapacity=RAM, tasksExecutionCapacity=TASK_EXECUTION_CAPACITY, cpuNumber=CPU_NUMBER)
 
+    # Creación de procesos
     for i in range(NUMBER_OF_PROCESSES):
+
         executionStart = random.expovariate(1.0 / INTERVAL)
         env.process(OS.newProcess(
             random.randint(1, 50), random.randint(1, 10), arrivalDelay=executionStart))
 
-        print(f"exec start {executionStart}")
     env.run()
 
     print("\n----------SIMULACION FINALIZADA----------\n")
+
     mean = np.mean(OS.executionsRecord)
     desv = np.std(OS.executionsRecord)
 
